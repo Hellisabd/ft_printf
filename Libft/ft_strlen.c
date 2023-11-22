bf016c8ade878387996d014e5b4c133a498832ba
Basile Grosjean <bgrosjea@k1r1p4.42mulhouse.fr>
2023-11-22 15:40:26 +0100

    protege

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgrosjea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bgrosjea <bgrosjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:17:35 by bgrosjea          #+#    #+#             */
-/*   Updated: 2023/11/04 17:04:32 by bgrosjea         ###   ########.fr       */
+/*   Updated: 2023/11/22 15:39:56 by bgrosjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	    if (!s)
+        return (NULL);
 	while (s[i] != '\0')
 		i++;
 	return (i);
